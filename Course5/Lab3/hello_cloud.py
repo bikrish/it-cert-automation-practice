7d68801183eee41986f60f49acbf65efe94bc34e
bikrishamatya <77570907+bikrishamatya@users.noreply.github.com>
2021-01-17 13:21:59 +0000

    Added a comment

--- Course5/Lab3/hello_cloud.py
+++ Course5/Lab3/hello_cloud.py
@@ -18,6 +18,8 @@ Optionally, a different port can be passed.
 
 The code was inspired by:
 https://gist.github.com/davidbgk/b10113c3779b8388e96e6d0c44e03a74
+
+Adding another line 
 """
 import http
 import http.server
